b2ce63532db85af0668edabf0773954c2676fd13
DemLoveSky <dem.loginov@gmail.com>
2022-02-14 06:24:26 +0500

    Add concept news card

---
+++ src/News/system/sysNews.js
@@ -0,0 +1,92 @@
+import {Box, Card, CardContent, CardMedia, Typography} from "@mui/material";
+
+export const SystemNews = (props) => {
+    return(
+        <Box>
+            <Card sx={{ mb: 2 }}>
+                <CardContent>
+                    <Typography variant="caption" color="text.secondary">#Тэг1 #Тэг2 #Тэг3</Typography>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Название статьи
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Card sx={{ mb: 2 }}>
+                <CardContent>
+                    <Typography variant="caption" color="text.secondary">#Тэг1 #Тэг2 #Тэг3</Typography>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Название статьи
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Card sx={{ mb: 2 }}>
+                <CardContent>
+                    <Typography variant="caption" color="text.secondary">#Тэг1 #Тэг2 #Тэг3</Typography>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Название статьи
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Card sx={{ mb: 2 }}>
+                <CardContent>
+                    <Typography variant="caption" color="text.secondary">#Тэг1 #Тэг2 #Тэг3</Typography>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Название статьи
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Card sx={{ mb: 2 }}>
+                <CardContent>
+                    <Typography variant="caption" color="text.secondary">#Тэг1 #Тэг2 #Тэг3</Typography>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Название статьи
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Card sx={{ mb: 2 }}>
+                <CardContent>
+                    <Typography variant="caption" color="text.secondary">#Тэг1 #Тэг2 #Тэг3</Typography>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Название статьи
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except AntarcticaLizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Box>
+    );
+};
